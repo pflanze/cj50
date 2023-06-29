d657b58c3a989930e03909d77f094021349c9ee7
Christian Jaeger <ch@christianjaeger.ch>
2023-06-29 14:09:58 +0200

    cj50: change all get_* functions to return Option

--- cj50.h
+++ cj50.h
@@ -14,6 +14,8 @@
 #include "cjmath.h"
 #include "sdlutil.h"
 #include "u8l/basic-util.h"
+#include "u8l/Option.h"
+
 
 
 void _print_debug_char(char c) {
@@ -53,6 +55,18 @@ void print_debug_char(char c) {
 
 typedef char* string;
 
+void drop_string(const string s) {
+    free(s);
+}
+
+// receive pointers to pointers just for standard in Option
+bool equal_string(const string *a, const string *b) {
+    return strcmp(*a, *b) == 0;
+}
+
+
+DEFTYPE_Option(string);
+
 void print_string(const char* str) {
     printf("%s", str);
 }
@@ -66,7 +80,7 @@ void print_debug_string(const char* str) {
     print_string("\"");
 }
 
-string get_string() {
+Option(string) get_string() {
     while (true) {
 #define SIZ 100
         char *line = malloc(SIZ);
@@ -75,8 +89,12 @@ string get_string() {
         errno = 0;
         ssize_t n = getline(&line, &len, stdin);
         if (n < 0) {
-            DIE_("Could not get a line from stdin: %s",
-                 errno == 0 ? "EOF" : strerror(errno));
+            if (errno == 0) {
+                return none_string();
+            } else {
+                DIE_("Could not get a line from stdin: %s",
+                     strerror(errno));
+            }
         }
         size_t l = strlen(line);
         // Always must have either a '\n' at the end or some other
@@ -87,24 +105,38 @@ string get_string() {
             l--;
         }
         if (l > 0) {
-            return line;
+            return some_string(line);
         }
         free(line);
         print_string("Your answer is empty. Please enter a string: ");
     }
 }
 
-int get_int() {
+
+static UNUSED
+bool equal_int(const int *a, const int *b) {
+    return *a == *b;
+}
+
+static UNUSED
+void drop_int(const int UNUSED a) {}
+
+DEFTYPE_Option(int);
+
+Option(int) get_int() {
     while (true) {
-        string s = get_string();
+        Option(string) s = get_string();
+        if (!s.is_some) {
+            return none_int();
+        }
         char *tail;
         errno = 0;
-        long n = strtol(s, &tail, 10);
+        long n = strtol(s.value, &tail, 10);
         if (errno == 0) {
             if (*tail == '\0') {
                 if (n >= INT_MIN && n <= INT_MAX) {
-                    free(s);
-                    return n;
+                    drop_Option_string(s);
+                    return some_int(n);
                 } else {
                     print_string("Your answer is not within the range of "
                                  "numbers of the `int` type.");
@@ -118,7 +150,7 @@ int get_int() {
                    "for the `long` type: %s.",
                    strerror(errno));
         }
-        free(s);
+        drop_Option_string(s);
         print_string(" Please enter an integer number: ");
     }
 }
@@ -137,10 +169,13 @@ void print_uint(uint n) {
 
 typedef int nat;
 
-int get_nat() {
+Option(int) get_nat() {
     while (true) {
-        int i = get_int();
-        if (i > 0) {
+        Option(int) i = get_int();
+        if (!i.is_some) {
+            return none_int();
+        }
+        if (i.value > 0) {
             return i;
         }
         print_string("Your answer is not above zero.");
@@ -159,10 +194,13 @@ void print_nat(int n) {
 
 typedef int nat0;
 
-int get_nat0() {
+Option(int) get_nat0() {
     while (true) {
-        int i = get_int();
-        if (i >= 0) {
+        Option(int) i = get_int();
+        if (!i.is_some) {
+            return none_int();
+        }
+        if (i.value >= 0) {
             return i;
         }
         print_string("Your answer is negative.");
@@ -179,21 +217,34 @@ void print_nat0(int n) {
 }
 
 
+static UNUSED
+void drop_float(float UNUSED x) { }
+
+static UNUSED
+bool equal_float(const float *a, const float *b) {
+    return *a == *b;
+}
+
+DEFTYPE_Option(float);
+
 void print_float(float x) {
     printf("%g", x);
 }
 
 // largely copy-paste of get_int
-float get_float() {
+Option(float) get_float() {
     while (true) {
-        string s = get_string();
+        Option(string) s = get_string();
+        if (!s.is_some) {
+            return none_float();
+        }
         char *tail;
         errno = 0;
-        float x = strtof(s, &tail);
+        float x = strtof(s.value, &tail);
         if (errno == 0) {
             if (*tail == '\0') {
-                free(s);
-                return x;
+                drop_Option_string(s);
+                return some_float(x);
             } else {
                 print_string("Please enter only a number with nothing "
                              "after it.");
@@ -203,7 +254,7 @@ float get_float() {
                    "possible range for the `float` type: %s.",
                    strerror(errno));
         }
-        free(s);
+        drop_Option_string(s);
         print_string(" Please enter a floating point number: ");
     }
 }
@@ -386,7 +437,7 @@ float* resize_floats(float* ary, size_t oldlen, size_t newlen) {
 
 #define drop(v)                                 \
     _Generic((v)                                \
-             , char*: free                      \
+             , char*: drop_string               \
              , int*: free                       \
              , float*: free                     \
              , Vec2*: free                    \
