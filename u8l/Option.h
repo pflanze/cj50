2cd215fd0907d522137c34d14038a6994f85f5d8
Christian Jaeger <ch@christianjaeger.ch>
2023-06-29 13:43:14 +0200

    Option: rename Option_ type constructor to Option
    I have decided to use lcfirst value constructor names now, which makes
    this unambiguous!

--- u8l/Option.h
+++ u8l/Option.h
@@ -11,40 +11,40 @@
 #include "macro-util.h"
 
 
-#define Option_(T) XCAT(Option_,T)
+#define Option(T) XCAT(Option_,T)
 
-#define DEFTYPE_Option_(T)                                      \
-    typedef struct Option_(T) {                                 \
+#define DEFTYPE_Option(T)                                       \
+    typedef struct Option(T) {                                  \
         bool is_none;                                           \
         T value;                                                \
-    } Option_(T);                                               \
+    } Option(T);                                                \
                                                                 \
     /* We don't drop the `value` as it may have */              \
     /* changed ownership in the mean time! */                   \
     static inline UNUSED                                        \
-    void XCAT(drop_, Option_(T))(const Option_(T) UNUSED s) { } \
+    void XCAT(drop_, Option(T))(const Option(T) UNUSED s) { }   \
                                                                 \
     static UNUSED                                               \
-    bool XCAT(equal_, Option_(T))(const Option_(T) *a,          \
-                                  const Option_(T) *b) {        \
+    bool XCAT(equal_, Option(T))(const Option(T) *a,            \
+                                 const Option(T) *b) {          \
         return ((a->is_none == b->is_none) &&                   \
                 (a->is_none ? true                              \
                  : XCAT(equal_, T)(&a->value, &b->value)));     \
     }                                                           \
                                                                 \
     static UNUSED                                               \
-    bool XCAT(is_some_, T)(const Option_(T) *s) {               \
+    bool XCAT(is_some_, T)(const Option(T) *s) {                \
         return !(s->is_none);                                   \
     }                                                           \
                                                                 \
     static UNUSED                                               \
-    Option_(T) XCAT(some_, T)(T val) {                          \
-        return (Option_(T)) { .is_none = false, .value = val }; \
+    Option(T) XCAT(some_, T)(T val) {                           \
+        return (Option(T)) { .is_none = false, .value = val };  \
     }                                                           \
                                                                 \
     static UNUSED                                               \
-    Option_(T) XCAT(none_, T)() {                               \
-        return (Option_(T)) { .is_none = false };               \
+    Option(T) XCAT(none_, T)() {                                \
+        return (Option(T)) { .is_none = false };                \
     }
 
 
