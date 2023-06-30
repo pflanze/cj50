556b35ff383d79bbb2db731c607502563c468af9
Christian Jaeger <ch@christianjaeger.ch>
2023-06-30 02:58:02 +0200

    Finish examples/graphics

--- examples/graphics.c
+++ examples/graphics.c
@@ -20,7 +20,8 @@ int main(/* int argc, char* args[] */) {
         DIE_("Window could not be created! SDL_Error: %s",
              SDL_GetError());
     }
-    
+
+    /*
     SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
 
     //Fill the surface white
@@ -30,7 +31,7 @@ int main(/* int argc, char* args[] */) {
     
     //Update the surface
     SDL_UpdateWindowSurface(window);
-
+    */
     SDL_Renderer * renderer = SDL_CreateRenderer(
         window,
         -1,
@@ -39,14 +40,31 @@ int main(/* int argc, char* args[] */) {
         DIE_("Accelerated renderer could not be created! SDL_Error: %s",
              SDL_GetError());
     }
-    assert_sdl(SDL_SetRenderDrawColor(renderer, 156,0,0,128));
-    SDL_Rect r = { 10, 20, 30, 40 };
-    assert_sdl(SDL_RenderDrawRect(renderer, &r) < 0);
 
-    //Hack to get window to stay up
+    
+    float t = 0;
+    
     SDL_Event e;
     bool quit = false;
     while (!quit) {
+
+        t += 0.01;
+        float factor = sin(t);
+
+        assert_sdl(SDL_SetRenderDrawColor(renderer, 156,0,0, 128));
+        assert_sdl(SDL_RenderClear(renderer));
+        assert_sdl(SDL_SetRenderDrawColor(renderer, 0,156,0, 128));
+
+        Vec2 diagonal = { 5, 5 };
+        for (int i = 0; i < 70; i++) {
+            Vec2 topleft = { 300 + i * factor * 4, i * 6 };
+            Rect2 r = { topleft, diagonal };
+            /* D(r); */
+            SDL_Rect sr = sdl(r);
+            assert_sdl(SDL_RenderDrawRect(renderer, &sr) < 0);
+        }
+        SDL_RenderPresent(renderer);
+
         while (SDL_PollEvent(&e)) {
             D(e.type);
             if (e.type == SDL_QUIT) {
@@ -67,6 +85,8 @@ int main(/* int argc, char* args[] */) {
                 }
             }
         }
+
+        
         /* print("Outer loop.\n"); */
         /* sleep(1); */
     }
