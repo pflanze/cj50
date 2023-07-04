571ef32c22df4105cdef265ed3353ca22b5c5172
Christian Jaeger <ch@christianjaeger.ch>
2023-07-04 04:57:29 +0200

    examples/plot: compile both plot_function* uses

--- examples/plot.c
+++ examples/plot.c
@@ -9,14 +9,17 @@ Option(float) sine(float x) {
 }
 
 int main() {
-    // plot_function_float(sine, rect2(vec2(-2, -2), vec2(4, 4)));
+    if (getenv("SINGLEFUNCTION")) {
+        plot_function_float(sine, rect2(vec2(-2, -2), vec2(4, 4)));
+    } else {
 
 #define NUMFS 2
-    ColorFunction_float fs[NUMFS] = {
-        { color(250, 80, 0), inverse },
-        { color(0, 120, 240), sine },
-    };
+        ColorFunction_float fs[NUMFS] = {
+            { color(250, 80, 0), inverse },
+            { color(0, 120, 240), sine },
+        };
 
-    plot_functions_float(NUMFS, fs, rect2(vec2(-2, -2), vec2(4, 4)));
+        plot_functions_float(NUMFS, fs, rect2(vec2(-2, -2), vec2(4, 4)));
 #undef NUMFS
+    }
 }
