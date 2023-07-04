72831c30e35c9f18b66e97f24bb0bbc15546a875
Christian Jaeger <ch@christianjaeger.ch>
2023-07-04 04:52:47 +0200

    plot: add `plot_functions_float`

--- cj50/plot.h
+++ cj50/plot.h
@@ -4,8 +4,17 @@
 #include <cj50/sdlutil.h>
 #include <cj50/float.h>
 
-struct PlotrenderCtx {
+
+/// Combination of color and a function, for drawing multiple
+/// functions in the same plot via `plot_functions_float`.
+typedef struct ColorFunction_float {
+    Color color;
     Option(float)(*f)(float);
+} ColorFunction_float;
+
+struct PlotrenderCtx {
+    size_t num_functions;
+    ColorFunction_float* functions;
     int width;
     int height;
     Rect2 viewport;
@@ -16,30 +25,60 @@ bool plot_render(SDL_Renderer* renderer, void* _ctx) {
     Rect2* viewport = &ctx->viewport;
     Vec2 start = viewport->start;
     Vec2 extent = viewport->extent;
-    Option(float)(*f)(float) = ctx->f;
 
     assert_sdl(SDL_SetRenderDrawColor(renderer, 0,0,0, 128));
     assert_sdl(SDL_RenderClear(renderer));
-    assert_sdl(SDL_SetRenderDrawColor(renderer, 240,220,0, 128));
 
     Vec2 diagonal = { 2, 2 };
     float w = ctx->width;
     float h = ctx->height;
-    for (int i = 0; i < ctx->width; i++) {
-        float x = start.x + extent.x / w * i;
-        Option(float) y = f(x);
-        if (y.is_some) {
-            /* D(vec2(x, y.value)); */
-            int yint = (y.value - start.y) / extent.y * h;
-            Vec2 pos = { i, ctx->height - yint };
-            /* D(pos); */
-            Rect2 r = { pos, diagonal };
-            SDL_Rect sr = to_sdl(r);
-            assert_sdl(SDL_RenderDrawRect(renderer, &sr));
+
+    for (size_t j = 0; j < ctx->num_functions; j++) {
+        Color color = ctx->functions[j].color;
+        Option(float)(*f)(float) = ctx->functions[j].f;
+        
+        assert_sdl(SDL_SetRenderDrawColor(renderer,
+                                          color.r,
+                                          color.g,
+                                          color.b,
+                                          128));
+
+        for (int i = 0; i < ctx->width; i++) {
+            float x = start.x + extent.x / w * i;
+            Option(float) y = f(x);
+            if (y.is_some) {
+                /* D(vec2(x, y.value)); */
+                int yint = (y.value - start.y) / extent.y * h;
+                Vec2 pos = { i, ctx->height - yint };
+                /* D(pos); */
+                Rect2 r = { pos, diagonal };
+                SDL_Rect sr = to_sdl(r);
+                assert_sdl(SDL_RenderDrawRect(renderer, &sr));
+            }
         }
     }
-
+    
     return true;
+}
+
+
+
+/// Show a window with a 2-dimensional plot of the `num_fs` given
+/// functions, which expect a float and return an optional float
+/// when possible. `viewport` is the (initial) range of coordinates
+/// that is shown on the screen.
+int plot_functions_float(size_t num_fs, ColorFunction_float* fs,
+                         Rect2 viewport) {
+    const int width = 800;
+    const int height = 600;
+
+    struct PlotrenderCtx ctx = {
+        num_fs, fs, width, height, viewport
+    };
+
+    graphics_render(width, height, plot_render, &ctx);
+
+    return 0;
 }
 
 
@@ -51,12 +90,18 @@ int plot_function_float(Option(float)(*f)(float), Rect2 viewport) {
     const int width = 800;
     const int height = 600;
 
+#define NUMFS 1
+    ColorFunction_float fs[NUMFS] = {
+        { color(240, 220, 0), f }
+    };
+
     struct PlotrenderCtx ctx = {
-        f, width, height, viewport
+        NUMFS, fs, width, height, viewport
     };
 
     graphics_render(width, height, plot_render, &ctx);
 
     return 0;
+#undef NUMFS
 }
 
--- examples/plot.c
+++ examples/plot.c
@@ -4,6 +4,19 @@ Option(float) inverse(float x) {
     return (x == 0) ? none(typeof(x)) : some(.01f / x);
 }
 
+Option(float) sine(float x) {
+    return some(sinf(x));
+}
+
 int main() {
-    plot_function_float(inverse, rect2(vec2(-2, -2), vec2(4, 4)));
+    // plot_function_float(sine, rect2(vec2(-2, -2), vec2(4, 4)));
+
+#define NUMFS 2
+    ColorFunction_float fs[NUMFS] = {
+        { color(250, 80, 0), inverse },
+        { color(0, 120, 240), sine },
+    };
+
+    plot_functions_float(NUMFS, fs, rect2(vec2(-2, -2), vec2(4, 4)));
+#undef NUMFS
 }
