f9b221d40d67d4472ae4fc4e8c7917593f34b636
Christian Jaeger <ch@christianjaeger.ch>
2023-07-06 04:43:02 +0200

    examples/random: using unwrap

--- examples/random.c
+++ examples/random.c
@@ -4,16 +4,23 @@ int main(int argc, string* argv) {
     if (argc != 2) {
         DIE("usage: random range");
     }
-    Result(int, ParseError) range = parse_int(argv[1]);
-    D(range);
-    if (! range.is_ok) {
-        DIE_("argument 1 %s", string_from_ParseError(range.err));
+    int range;
+    if (getenv("EXPLICIT")) {
+        Result(int, ParseError) _range = parse_int(argv[1]);
+        D(_range);
+        if (! _range.is_ok) {
+            DIE_("argument 1 %s", string_from_ParseError(_range.err));
+        }
+        range = _range.ok;
+    } else {
+        range = unwrap(parse_int(argv[1]));
     }
+
     for (int i = 0; i < 10; i++) {
-        int r = get_random_int(range.ok);
+        int r = get_random_int(range);
         print(r);
         print("\n");
         assert(r >= 0);
-        assert(r < range.ok);
+        assert(r < range);
     }
 }
