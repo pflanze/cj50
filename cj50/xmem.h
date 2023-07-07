a1eb65520aa335b084c53798584a0ef063567df5
Christian Jaeger <ch@christianjaeger.ch>
2023-07-07 14:54:18 +0200

    Add mem.h from utf-8-lineseparator

---
+++ cj50/xmem.h
@@ -0,0 +1,63 @@
+/*
+  Copyright (C) 2021 Christian Jaeger, <ch@christianjaeger.ch>
+  Published under the terms of the MIT License, see the LICENSE file.
+*/
+
+#ifndef MEM_H_
+#define MEM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static
+void die_outofmemory() {
+    fprintf(stderr, "Out of memory, aborting\n");
+    abort();
+}
+
+static inline
+void *xmalloc(size_t size) {
+    void *p = malloc(size);
+    if (!p) die_outofmemory();
+    return p;
+}
+
+static inline
+void *xmallocarray(size_t nmemb, size_t size) {
+    size_t bytes = nmemb * size;
+    if ((bytes < nmemb) || (bytes < size))
+        die_outofmemory();
+    void *p = xmalloc(bytes);
+    if (!p) die_outofmemory();
+    return p;
+}
+
+static inline
+void *xreallocarray(void *ptr, size_t nmemb, size_t size) {
+    // void *p = reallocarray(ptr, nmemb, size);
+    size_t bytes = nmemb * size;
+    if ((bytes < nmemb) || (bytes < size))
+        die_outofmemory();
+    void *p = realloc(ptr, bytes);
+    if (!p) die_outofmemory();
+    return p;
+}
+
+
+static
+char *xstrdup(const char *str) {
+    char *res= strdup(str);
+    if (!res) die_outofmemory();
+    return res;
+}
+
+
+void *xmemcpy(const void *src, size_t n) {
+    void *p = xmalloc(n);
+    memcpy(p, src, n);
+    return p;
+}
+
+
+#endif /* MEM_H_ */
