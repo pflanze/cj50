b1b32ca9bc5bed5b97d08a780586f3d05a4f84fc
Christian Jaeger <ch@christianjaeger.ch>
2023-08-08 22:17:32 +0200

    Add u32.h

---
+++ cj50/u32.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cj50/gen/Option.h>
+
+/// We alias this lengthy C type name to the shorter naming used in
+/// the Rust programming language.
+
+/// (This is especially helpful to preserve the use of "_" for
+/// appending type names in combined types.)
+
+typedef uint32_t u32;
+
+
+static UNUSED
+bool equal_u32(const u32 *a, const u32 *b) {
+    return *a == *b;
+}
+
+static UNUSED
+bool equal_move_u32(const u32 a, const u32 b) {
+    return a == b;
+}
+
+static UNUSED
+void drop_u32(const u32 UNUSED a) {}
+
+static UNUSED
+int print_u32(u32 n) {
+    return printf("%u", n);
+}
+
+static UNUSED
+int print_debug_u32(const u32 *n) {
+    return printf("%u", *n);
+}
+
+GENERATE_Option(u32);
+
