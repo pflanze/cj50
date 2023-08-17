eeab6e14d471442bae0a92513f2a71c2a666bf8b
Christian Jaeger <ch@christianjaeger.ch>
2023-08-17 22:46:44 +0200

    Create examples/snail

---
+++ examples/snail.c
@@ -0,0 +1,55 @@
+#include <cj50.h>
+
+
+/// -1..1 -> -1..1
+Option(float) circle_y(float x) {
+    float y = sinf(acosf(x));
+    return isnan(y) ? None(float) : some(y);
+}
+
+// one side of the body line
+Option(float) snail_y(float x) {
+    if_let_Some(y, circle_y(x)) {
+        return some(y * -(x - 3.0f) / 16.0f);
+    } else_None {
+        return None(float);
+    }
+}
+
+bool render_snail(SDL_Renderer *rdr, void *ctx) {
+    float *t = ctx;
+    *t += 0.04;
+
+    set_draw_color(rdr, color(0, 156, 0));
+    clear(rdr);
+    set_draw_color(rdr, color(210, 200, 100));
+
+    float x = -1.01;
+    for (int i = 0; i < 480; i++) {
+        if_let_Some(y, snail_y(x)) {
+            // adding waves
+            float yi = y * (200 + sinf(-*t + x * 40) * 5.);
+            draw_rect(rdr, rect2(vec2(100. + i, 240. + yi),
+                                 vec2(1, -yi * 2.)));
+        } else_None;
+        x += 2./400.;
+    }                      
+    return true;
+}
+
+Result(Unit, String) run(UNUSED slice(cstr) argv) {
+    BEGIN_Result(Unit, String);
+
+    if (getenv("PLOT")) {
+        plot_function_float(circle_y, rect2(vec2(-1.4, -1.1), vec2(2.8, 2.2)));
+    } else {
+        float t = 0.0;
+        graphics_render("Snail", 640, 480, render_snail, &t);
+    }
+
+    RETURN_Ok(Unit(), cleanup);
+cleanup:
+    END_Result();
+}
+
+MAIN(run);
