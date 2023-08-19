7ce0d4607d7862c045851ed5dcd4431a0c707639
Christian Jaeger <ch@christianjaeger.ch>
2023-08-19 03:14:29 +0200

    slices.h: doc fixes

--- cj50/gen/template/slices.h
+++ cj50/gen/template/slices.h
@@ -10,10 +10,11 @@
 /// Create a new slice from pointer `ptr` and number of elements
 /// pointed at `len`. Make sure that `len` is within the range of
 /// elements from the pointer! The slice borrows the storage, so make
-/// sure its life time is at least the one of the slice.
+/// sure the latter's life time is at least the one of the slice.
 
-/// Note that `slice(char)` isn't necessarily holding UTF-8 encoded
-/// data! (Todo: add a `str` wrapper which guarantees that.)
+/// (Note that a `slice(char)` isn't necessarily holding UTF-8 encoded
+/// data! See `strslice` for a slice that does.)
+
 static UNUSED
 SLICE(T) XCAT(new_, SLICE(T))(POSSIBLY_CONST T *ptr, size_t len) {
     return (SLICE(T)) {
