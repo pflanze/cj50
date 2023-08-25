4add2012ed5bfc07e10a58ae4c6ae4f1b5397623
Christian Jaeger <ch@christianjaeger.ch>
2023-08-26 01:45:05 +0200

    examples/vertexgraphics: make diamond top and bottom vertices move, too

--- examples/vertexgraphics.c
+++ examples/vertexgraphics.c
@@ -72,33 +72,54 @@ bool render(SDL_Renderer* renderer, void* _ctx, Vec2(int) window_dimensions) {
         wave(t, vec2_float(0.23, 0.5), vec2_float(9.3, 0.7), 0, 255),
         255);
     SDL_Color c2 = ColorA(250, 200, 30, 255);
-    int v1 = push_vertex(&ctx->rdr, vertex_2(vec2_float(90, 100), c1));
+#define X(v) ((v)*window_dimensions.x)
+#define Y(v) ((v)*window_dimensions.y)
+#define POS(x, y) vec2_float(X(x), Y(y))
+    int v1 = push_vertex(&ctx->rdr,
+                         vertex_2(vec2_float(wavef(t,
+                                                   vec2_float(11, 1),
+                                                   vec2_float(5.53, 1),
+                                                   X(.12), X(.16)),
+                                             wavef(t,
+                                                   vec2_float(13.2, 1),
+                                                   vec2_float(6.61, 1),
+                                                   Y(.19), Y(.23))),
+                                  c1));
     int v2 = push_vertex(&ctx->rdr,
                          vertex_2(vec2_float(wavef(t,
                                                    vec2_float(12, 1),
                                                    vec2_float(11.123, 1.1),
-                                                   340, 440),
+                                                   X(.53), X(.68)),
                                              wavef(t,
                                                    vec2_float(11.913, 1),
                                                    vec2_float(8.71, 0.5),
-                                                   160, 240)),
+                                                   Y(.33), Y(.5))),
                                   c2));
     int v3 = push_vertex(&ctx->rdr,
                          vertex_2(vec2_float(wavef(t,
                                                    vec2_float(12, 1),
                                                    vec2_float(11.123, 1.1),
-                                                   140, 220),
+                                                   X(.21), X(.34)),
                                              wavef(t,
                                                    vec2_float(11.913, 1),
                                                    vec2_float(8.71, 0.5),
-                                                   260, 340)),
+                                                   Y(.54), Y(.71))),
                                   c2));
     push_triangle(&ctx->rdr, vec3_int(v1, v2, v3));
     SDL_Color c3 = ColorA(255 - c1.r,
                           255 - c1.g,
                           255 - c1.b,
                           255);
-    int v4 = push_vertex(&ctx->rdr, vertex_2(vec2_float(500, 380), c3));
+    int v4 = push_vertex(&ctx->rdr,
+                         vertex_2(vec2_float(wavef(t,
+                                                   vec2_float(11, 1),
+                                                   vec2_float(5.53, 1),
+                                                   X(.76), X(.80)),
+                                             wavef(t,
+                                                   vec2_float(13.2, 1),
+                                                   vec2_float(6.61, 1),
+                                                   Y(.77), Y(.81))),
+                                  c3));
     push_triangle(&ctx->rdr, vec3_int(v2, v3, v4));
 
     // Floating triangles:
