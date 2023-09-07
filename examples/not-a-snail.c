8a2ec5c9d013b96faa1b2bdfd6947183d58f8edc
Christian Jaeger <ch@christianjaeger.ch>
2023-09-07 04:39:27 +0200

    examples/not-a-snail: take NTH env var to only draw every nth frame

--- examples/not-a-snail.c
+++ examples/not-a-snail.c
@@ -16,24 +16,36 @@ Option(float) snail_y(float x) {
     }
 }
 
-bool render_snail(SDL_Renderer *rdr, void *ctx, Vec2(int) window_dimensions) {
-    float *t = ctx;
-    *t += 0.04;
+typedef struct Ctx {
+    float t;
+    u32 framenumber;
+    int draw_every_nth_frame;
+} Ctx;
+
+bool render_snail(SDL_Renderer *rdr, void *_ctx, Vec2(int) window_dimensions) {
+    Ctx* ctx = _ctx;
+    ctx->t += 0.04;
 
     set_draw_color(rdr, color(100, 140, 246));
     clear(rdr);
     set_draw_color(rdr, color(110, 100, 50));
 
-    float x = -1.01;
-    for (int i = 0; i < 480; i++) {
-        if_let_Some(y, snail_y(x)) {
-            // adding waves
-            float yi = y * (200 + sinf(-*t + x * 40) * 5.);
-            draw_rect(rdr, rect2(vec2_float(100. + i, 240. + yi),
-                                 vec2_float(1, -yi * 2.)));
-        } else_None;
-        x += 2./400.;
-    }                      
+    if (ctx->framenumber == 0) {
+        ctx->framenumber = ctx->draw_every_nth_frame;
+
+        float x = -1.01;
+        for (int i = 0; i < 480; i++) {
+            if_let_Some(y, snail_y(x)) {
+                // adding waves
+                float yi = y * (200 + sinf(-ctx->t + x * 40) * 5.);
+                draw_rect(rdr, rect2(vec2_float(100. + i, 240. + yi),
+                                     vec2_float(1, -yi * 2.)));
+            } else_None;
+            x += 2./400.;
+        }
+    }
+    ctx->framenumber--;
+
     return true;
 }
 
@@ -52,8 +64,20 @@ Result(Unit, String) run(UNUSED slice(cstr) argv) {
         plot_functions_float(fs, rect2(vec2_float(-1.4, -1.1),
                                        vec2_float(2.8, 2.2)));
     } else {
-        float t = 0.0;
-        graphics_render("Not a snail", vec2_int(640, 480), render_snail, &t);
+        int draw_every_nth_frame;
+        char *nth = getenv("NTH");
+        if (nth) {
+            draw_every_nth_frame = TRY(parse_nat(nth), cleanup);
+        } else {
+            draw_every_nth_frame = 1;
+        }
+
+        Ctx ctx = {
+            0.,
+            0,
+            draw_every_nth_frame
+        };
+        graphics_render("Not a snail", vec2_int(640, 480), render_snail, &ctx);
     }
 
     RETURN_Ok(Unit(), cleanup);
