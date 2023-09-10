4d8f17d8c14d74b75827fb0a32f978ef0a23a10e
Christian Jaeger <ch@christianjaeger.ch>
2023-09-11 01:57:06 +0200

    examples/draw_circle: ellispes

--- examples/draw_circle.c
+++ examples/draw_circle.c
@@ -1,10 +1,41 @@
 #include <cj50.h>
 
+static
+void render1(bool do_rect, VertexRenderer *vertexrenderer,
+             SDL_Renderer *rdr, Vec2(int) window_dimensions) {
+    int x = 10;
+    for (int i = 0; ; i++) {
+        int diameter = square(i);
+        int next_x = x + 10 + diameter;
+        if (next_x > window_dimensions.x) {
+            break;
+        }
+        float diameter_y = diameter * (1.2f + 1.1f * sinf(i));
+        AUTO r = rect2_float(vec2_float(x, window_dimensions.y - 10 - diameter_y),
+                             vec2_float(diameter, diameter_y));
+        if (do_rect) {
+            draw_fill_rect(rdr, r);
+        } else {
+            // XX A not working
+            draw_fill_ellipsis(vertexrenderer,
+                               r,
+                               ColorA(30, 20, 240, 255));
+        }
+        x = next_x;
+    }
+}
+
+
 bool render(SDL_Renderer *rdr, UNUSED void *_ctx, Vec2(int) window_dimensions) {
     set_draw_color(rdr, color(30, 240, 40));
     clear(rdr);
     set_draw_color(rdr, color(240, 30, 10));
     draw_fill_circle(rdr, vec2_int(500, 400), 120);
+    AUTO vertexrenderer = new_VertexRenderer();
+    render1(true, &vertexrenderer, rdr, window_dimensions);
+    render1(false, &vertexrenderer, rdr, window_dimensions);
+    render_VertexRenderer(rdr, &vertexrenderer);
+    drop(vertexrenderer);
     return true;
 }
 
